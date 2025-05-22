c041133099474519e64bcb8741fb40de153b0e9c
LucasFerraz07 <lucas.meirelles0411@gmail.com>
2025-05-21 21:58:46 -0300

    atualização requirements + criação dos managers

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Projeto-Final-Python-SENAI
 Projeto Final do curso de programação em python do SENAI
+
+# ERRO no import
+Ao invés de utilizar o comando "pip install", instale os pacotes um por um na ordem mencionada do "requirements.txt" 
--- main.py
+++ main.py
@@ -1,12 +1,64 @@
 import pygame
+import pygame_gui
+import pygame_gui.ui_manager
 
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+
+largura_tela = 800
+altura_tela = 800
+tela = pygame.display.set_mode((largura_tela, altura_tela))
+pygame.display.set_caption('Jogo da cobrinha')
+
+# criando manager para o pygame_gui
+gerente_menu = pygame_gui.UIManager((largura_tela, altura_tela), 'theme.json')
+gerente_inicio = pygame_gui.UIManager((largura_tela, altura_tela), 'theme.json')
+gerente_jogando = pygame_gui.UIManager((largura_tela, altura_tela), 'theme.json')
+gerente_score = pygame_gui.UIManager((largura_tela, altura_tela), 'theme.json')
+
+# pinta a tela
+# Usa o RGB como padrão 
+tela_cor = (0, 0, 0)
+tela.fill(tela_cor)
+
+#Criação botão menu:
+menu_button_facil = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect([(350, 350), (100, 50)]),
+        text='FÁCIL',
+        manager = gerente_menu
+    )
+
+menu_button_medio = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect([(350, 401), (100, 50)]),
+        text='MÉDIO',
+        manager = gerente_menu
+    )
+
+menu_button_dificil = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect([(350, 451), (100, 50)]),
+        text='DIFÍCIL',
+        manager = gerente_menu
+    )
+
+#Criação botão inicio:
+start_button = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect([(350, 350), (100, 50)]),
+        text='Start Game',
+        manager = gerente_inicio
+    )
+
+# estados do jogo:
+# menu, inicio, jogando, score, fim
+estado = 'menu'
+
 running = True
 
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
+        gerente_menu.process_events(event)
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame_gui.UI_BUTTON_PRESSED:
+            if event.ui_element == start_button:
+                estado = 'jogando'
