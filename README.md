238efdb17867280e29e3772a1b0f2feb8e97319b
LucasFerraz07 <lucas.meirelles0411@gmail.com>
2025-05-23 21:05:37 -0300

    botoões MENU + botão INICIO

--- README.md
+++ README.md
@@ -3,3 +3,13 @@ Projeto Final do curso de programação em python do SENAI
 
 # ERRO no import
 Ao invés de utilizar o comando "pip install", instale os pacotes um por um na ordem mencionada do "requirements.txt" 
+
+# How to
+Para começar a rodar o projeto, crie um venv com o comando:
+python -m venv venv
+
+Ative o venv:
+venv\Scripts\activate
+
+Instale as dependências:
+pip install -r requirements.txt
--- main.py
+++ main.py
@@ -10,10 +10,7 @@ tela = pygame.display.set_mode((largura_tela, altura_tela))
 pygame.display.set_caption('Jogo da cobrinha')
 
 # criando manager para o pygame_gui
-gerente_menu = pygame_gui.UIManager((largura_tela, altura_tela), 'theme.json')
-gerente_inicio = pygame_gui.UIManager((largura_tela, altura_tela), 'theme.json')
-gerente_jogando = pygame_gui.UIManager((largura_tela, altura_tela), 'theme.json')
-gerente_score = pygame_gui.UIManager((largura_tela, altura_tela), 'theme.json')
+gerente = pygame_gui.UIManager((largura_tela, altura_tela), 'theme.json')
 
 # pinta a tela
 # Usa o RGB como padrão 
@@ -24,31 +21,29 @@ tela.fill(tela_cor)
 menu_button_facil = pygame_gui.elements.UIButton(
         relative_rect=pygame.Rect([(350, 350), (100, 50)]),
         text='FÁCIL',
-        manager = gerente_menu
+        manager = gerente
     )
 
 menu_button_medio = pygame_gui.elements.UIButton(
         relative_rect=pygame.Rect([(350, 401), (100, 50)]),
         text='MÉDIO',
-        manager = gerente_menu
+        manager = gerente
     )
 
 menu_button_dificil = pygame_gui.elements.UIButton(
         relative_rect=pygame.Rect([(350, 451), (100, 50)]),
         text='DIFÍCIL',
-        manager = gerente_menu
+        manager = gerente
     )
 
-#Criação botão inicio:
-start_button = pygame_gui.elements.UIButton(
-        relative_rect=pygame.Rect([(350, 350), (100, 50)]),
-        text='Start Game',
-        manager = gerente_inicio
-    )
 
 # estados do jogo:
-# menu, inicio, jogando, score, fim
+# menu, #inicio, jogando, score, fim
 estado = 'menu'
+
+#dificuldade do jogo:
+#facil, medio, dificil
+dificuldade = 'FACIL'
 
 running = True
 
@@ -56,9 +51,57 @@ while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
-        gerente_menu.process_events(event)
+        gerente.process_events(event)
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame_gui.UI_BUTTON_PRESSED:
-            if event.ui_element == start_button:
+            if event.ui_element == menu_button_facil:
+                estado = 'inicio'
+                dificuldade = 'FACIL' 
+                menu_button_facil.kill()
+                menu_button_medio.kill()
+                menu_button_dificil.kill()
+                #botão start
+                inicio_button = pygame_gui.elements.UIButton(
+                relative_rect=pygame.Rect([(350, 350), (100, 50)]),
+                text='START GAME',
+                manager = gerente
+                )
+            elif event.ui_element == menu_button_medio:
+                estado = 'inicio'
+                dificuldade = 'MEDIO' 
+                menu_button_facil.kill()
+                menu_button_medio.kill()
+                menu_button_dificil.kill()
+                #botão start
+                inicio_button = pygame_gui.elements.UIButton(
+                relative_rect=pygame.Rect([(350, 350), (100, 50)]),
+                text='START GAME',
+                manager = gerente
+                )
+            elif event.ui_element == menu_button_dificil:
+                estado = 'inicio'
+                dificuldade = 'DIFICIL' 
+                menu_button_facil.kill()
+                menu_button_medio.kill()
+                menu_button_dificil.kill()
+                #botão start
+                inicio_button = pygame_gui.elements.UIButton(
+                relative_rect=pygame.Rect([(350, 350), (100, 50)]),
+                text='START GAME',
+                manager = gerente
+                )
+            elif event.ui_element == inicio_button:
                 estado = 'jogando'
+                inicio_button.kill()
+
+    tela.fill(tela_cor)
+
+
+    # estou pedindo para atualizar os elementos do pygame_gui    
+    gerente.update(1 / 60.0)
+    gerente.draw_ui(tela)
+    
+    pygame.display.flip()
+
+pygame.quit()
